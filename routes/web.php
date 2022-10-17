bbc1713a7dbf9e7a76c71e3182cb933afcd6dc7b
gea-ryuza <3103120201@student.smktelkom-pwt.sch.id>
2022-10-17 10:52:46 +0700

    add api & endpoint

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/coba', function () {
+    return 'Hallo';
+});
+
+Route::get('/coba1', function () {
+    return ['rafa', 'rafa', 'rafa'];
+});
+
+Route::get('/coba2', function () {
+    return [
+        "Nama" => "Ryuza Aly Syahputra",
+        "NIS" => 3103120201,
+        "Kelas" => "XI RPL 6"
+    ];
+});
+
+Route::get('/coba3', function () {
+    return response()->json(
+        [
+            "Nama" => "Ryuza Aly Syahputra",
+            "NIS" => 3103120201,
+            "Kelas" => "XI RPL 6"
+        ],201
+    );
+});
